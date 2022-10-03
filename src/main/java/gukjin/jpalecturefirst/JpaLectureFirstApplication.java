c0b098d97366d51307e8cbf51ca4649fd9645756
gukjin <internet2dot0@gmail.com>
2022-10-03 20:30:05 +0900

    new JpaLectureFirst();

---
+++ src/main/java/gukjin/jpalecturefirst/JpaLectureFirstApplication.java
@@ -0,0 +1,13 @@
+package gukjin.jpalecturefirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLectureFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaLectureFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gukjin/jpalecturefirst/JpaLectureFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package gukjin.jpalecturefirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLectureFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
