97cb6fb86a643f82451262cc1306720dcae9dc38
=gukjin <internet2dot0@gmail.com>
2022-10-17 23:06:26 +0900

    Feat: view template and bootstrap

--- src/main/java/gukjin/jpa/controller/HelloController.java
+++ src/main/java/gukjin/jpa/controller/HelloController.java
@@ -1,15 +1,18 @@
 package gukjin.jpa.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class HelloController {
 
-    @GetMapping("hello")
+    @RequestMapping("/")
     private String hello(Model model){
-        model.addAttribute("data", "hello");
-        return "hello";
+        log.info("homeController passed");
+        return "home";
     }
 }
