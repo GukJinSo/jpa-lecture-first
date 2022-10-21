f0be9e7876baae458e43f2bc86704c072ad01db9
gukjin <internet2dot0@gmail.com>
2022-10-21 20:42:23 +0900

    Feat: GetMapping members

--- src/main/java/gukjin/jpa/api/MemberApiController.java
+++ src/main/java/gukjin/jpa/api/MemberApiController.java
@@ -3,20 +3,43 @@ package gukjin.jpa.api;
 import gukjin.jpa.domain.Address;
 import gukjin.jpa.domain.Member;
 import gukjin.jpa.service.MemberService;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream().map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect.size(), collect);
+    }
+
+    @Data @AllArgsConstructor @Setter @Getter
+    static class Result<T>{
+        private int count;
+        private T data;
+    }
+
+    @Data @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse savedMemberV1(@RequestBody @Valid Member member){ // json으로 온 body data를 바로 Member에 넣겠다
