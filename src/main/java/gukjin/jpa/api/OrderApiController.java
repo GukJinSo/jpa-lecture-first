59359cc68966f24245bb1c9f720fb81a31092fd8
=gukjin <internet2dot0@gmail.com>
2022-10-24 22:48:25 +0900

    Test: jpa paging error

--- src/main/java/gukjin/jpa/api/OrderApiController.java
+++ src/main/java/gukjin/jpa/api/OrderApiController.java
@@ -1,15 +1,22 @@
 package gukjin.jpa.api;
 
+import gukjin.jpa.domain.Address;
 import gukjin.jpa.domain.Order;
 import gukjin.jpa.domain.OrderItem;
+import gukjin.jpa.domain.OrderStatus;
+import gukjin.jpa.domain.item.Item;
 import gukjin.jpa.repository.OrderRepository;
 import gukjin.jpa.repository.OrderSearch;
 import gukjin.jpa.repository.order.simplequery.OrderSimpleQueryRepository;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.PublicKey;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,17 +25,64 @@ public class OrderApiController {
     private final OrderRepository orderRepository;
     private final OrderSimpleQueryRepository orderQueryRepository;
 
+
+    /**
+     * 강제 초기화를 통한 엔티티 직접 반환 쿼리
+     */
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
-        /**
         for (Order order : all) {
             order.getMember().getName(); //Lazy 강제 초기화
             order.getDelivery().getAddress(); //Lazy 강제 초기환
             List<OrderItem> orderItems = order.getOrderItems();
             orderItems.stream().forEach(o -> o.getItem().getName()); //Lazy 강제 초기화
         }
-         */
         return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        return orderRepository.findAllByString(new OrderSearch())
+                .stream().map(order -> new OrderDto(order)).collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        return orderRepository.findAllWithItem(new OrderSearch())
+                .stream().map(order -> new OrderDto(order)).collect(Collectors.toList());
+    }
+
+    @Getter
+    static class OrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto{
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem){
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
     }
 }
--- src/main/java/gukjin/jpa/repository/OrderRepository.java
+++ src/main/java/gukjin/jpa/repository/OrderRepository.java
@@ -11,6 +11,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
@@ -105,4 +106,16 @@ public class OrderRepository {
                 .getResultList();
     }
 
+    public List<Order> findAllWithItem(OrderSearch orderSearch) {
+        List<Order> resultList = em.createQuery("select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item it", Order.class)
+                .getResultList();
+        for (Order order : resultList) {
+            System.out.println("order = "+order+" , order.getId = "+order.getId());
+        }
+        return resultList;
+    }
 }
